b950ed7509a80c88db50d9b089ffd7f112c1b5ba
Eric <theericlau@gmail.com>
2018-04-17 22:20:36 -0700

    created routing and modularized the templates

--- djangoTodoList/todos/views.py
+++ djangoTodoList/todos/views.py
@@ -4,6 +4,20 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
 # Create your views here.
 def index(request):
-  return HttpResponse('Hello World')
+  todos = Todo.objects.all()[:10]
+  context = {
+    'todos': todos
+  }
+  # return HttpResponse('Hello World')
+  return render(request, 'index.html', context);
+
+def details(request, id):
+  todo = Todo.objects.get(id=id)
+  context = {
+    'todo': todo
+  }
+  # return HttpResponse('Hello World')
+  return render(request, 'details.html', context);
